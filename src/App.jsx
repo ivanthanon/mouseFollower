3d0124fd10bbc6c27a20c9510c20b647d10a00d5
Ivan Thanon <ivanmoreno199518@gmail.com>
2023-08-24 01:35:03 +0100

    first version

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import './App.css'
+import { useEffect, useState } from "react"
+
+const FollowMouse = () => {
+    const [enabled, setEnabled] = useState(false);
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+
+    useEffect(() => {
+      console.log("effect", { enabled });
+
+      const handleMove = (event) => {
+        const { clientX, clientY } = event;
+        setPosition({ x: clientX, y: clientY });
+      };
+
+      if (enabled) window.addEventListener("pointermove", handleMove);
+
+      return () => {
+        window.removeEventListener("pointermove", handleMove);
+      };
+    }, [enabled]);
+
+    return (
+      <>
+        <main>
+          {enabled && (
+            <div
+              style={{
+                position: "absolute",
+                backgroundColor: "rgb(255, 255, 0)",
+                border: "1px solid #fff",
+                borderRadius: "50%",
+                opacity: 0.8,
+                pointerEvents: "none",
+                left: -25,
+                top: -25,
+                width: 50,
+                height: 50,
+                transform: `translate(${position.x}px,${position.y}px)`,
+              }}
+            />
+          )}
+
+          {!enabled && (
+            <div
+              style={{
+                position: "absolute",
+                backgroundColor: "rgb(255, 255, 0)",
+                border: "1px solid #fff",
+                borderRadius: "50%",
+                opacity: 0.8,
+                pointerEvents: "none",
+                display: 'none',
+                left: -25,
+                top: -25,
+                width: 50,
+                height: 50,
+                transform: `translate(${position.x}px,${position.y}px)`,
+              }}
+            />
+          )}
+
+          <button
+            onClick={() => {
+              setEnabled(!enabled);
+            }}
+          >
+            {enabled
+              ? "Desactivar seguimiento de puntero"
+              : "Activar seguimiento de puntero"}
+          </button>
+        </main>
+      </>
+    );
+  
+}
+
+function App() {
+  return (
+    <FollowMouse></FollowMouse>
+  )
+}
+
+export default App
